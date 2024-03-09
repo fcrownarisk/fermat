0acbefc07b4f883db0b43818aaf75063f8471c3c
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-03-09 09:57:43 +0800

    Add files via upload

---
+++ fermat/fermat.ts
@@ -0,0 +1,15 @@
+import './tensor/BigXtensor'
+import './tensor/BigYtensor' 
+import './tensor/BigZtensor' 
+import './tensor/smallxtensor' 
+import './tensor/smallytensor'
+import './tensor/smallztensor'
+import './tensor/midxtensor' 
+import './tensor/midytensor'
+import './tensor/midztensor'
+void function fermat() {
+   void  ['BIgXtensor' + `BigYtensor` + `BigZtensor` === `smallxtensor` + `smallytensor` + `smallztensor`]
+   void  ['BIgXtensor - smallxtensor' + 'BigYtensor - smallytensor' +'BigZtensor - smallztensor'==='midxtensor'+'midytensor'+'midztensor']
+   void  ['BigXtensor * BigYtensor * BigZtensor'+'smallxtensor%smallytensor%mallztensor'==='0']
+    return fermat
+}
